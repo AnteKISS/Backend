d5ccd42776c94961bc0307df1deb4f4ec05bb237
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2024-07-18 01:04:52 -0400

    Format init.sql

--- Database/Scripts/init.sql
+++ Database/Scripts/init.sql
@@ -1,13 +1,22 @@
+-- Script : init.sql
 
-
+/******************************************/
+/***        DATABASE STRUCTURE          ***/
+/******************************************/
 \include Creation/CreateTables.sql;
 
+/******************************************/
+/***        STORED PROCEDURES           ***/
+/******************************************/
+
+-- Character/Stats
 \include Stored_Procs/PROC_AddAttribute.sql
 \include Stored_Procs/PROC_AddClass.sql
 \include Stored_Procs/PROC_AddClassAttribute.sql
 \include Stored_Procs/PROC_AddClassStat.sql
 \include Stored_Procs/PROC_AddStat.sql
 
+-- Items
 \include Stored_Procs/PROC_AddItem.sql
 \include Stored_Procs/PROC_AddItemBaseStats.sql
 \include Stored_Procs/PROC_AddItemFunction.sql
@@ -21,18 +30,29 @@
 \include Stored_Procs/PROC_AddItemType.sql
 \include Stored_Procs/PROC_LinkItemSlotFunction.sql
 
+-- Monsters
 \include Stored_Procs/PROC_AddMonster.sql
 \include Stored_Procs/PROC_AddBaseMonster.sql
 
+-- Data Insertion
 \include Stored_Procs/PROC_InsertItems.sql
 
+/******************************************/
+/***             FUNCTIONS              ***/
+/******************************************/
+
+-- Items
 \include Functions/FUNC_GetAllItemsCore.sql
 \include Functions/FUNC_GetItemCore.sql
 \include Functions/FUNC_GetItemModifiers.sql
 \include Functions/FUNC_GetItemStats.sql
 
+-- Monsters
 \include Functions/FUNC_GetAllMonsters.sql
 \include Functions/FUNC_GetMonsterModifiers.sql
 \include Functions/FUNC_GetMonsterSkills.sql
 
+/******************************************/
+/***           DATA INSERTION           ***/
+/******************************************/
 \include Creation/InsertData.sql
